4a2f6f3f9c573af96b6a0d865c61b3bfbc580a30
sebastian hadley <sebastian.hadley@hotmail.com>
2022-08-25 22:27:29 +1000

    tax stuff done

--- ServerSample.java
+++ ServerSample.java
@@ -12,31 +12,28 @@ public class ServerSample {
 
 
     public static void main(String[] args) {
-       ServerSample server = new ServerSample(4500);
-       server.go();
+        ServerSample server = new ServerSample(4500);
+        server.go();
     }
 
     //Constructor so that not static methods can be used.
-    public ServerSample(int socket)
-    {
+    public ServerSample(int socket) {
         socketnumber = socket;
     }
 
     //used to allow use of non static methods.
-    public void go()
-    {
+    public void go() {
         String msg;
         try (
                 ServerSocket ss = new ServerSocket(4500);
                 Socket s = ss.accept();
                 BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
                 PrintWriter out = new PrintWriter(s.getOutputStream(), true)
-        )
-        {
-            while (!(msg = in.readLine()).equals("TAX"));
+        ) {
+            while (!(msg = in.readLine()).equals("TAX")) ;
             outputMessage(msg);
             out.println("TAX: OK");
-            while(s.isConnected()) {
+            while (s.isConnected()) {
                 {
                     switch (msg = in.readLine()) {
                         case "BYE":
@@ -46,17 +43,20 @@ public class ServerSample {
                             System.out.println("hello");
                             String[] taxInfo = new String[4];
                             for (int i = 0; i < 4; i++) {
-                                while ((msg = in.readLine()).isBlank());
+                                while ((msg = in.readLine()).isBlank()) ;
                                 taxInfo[i] = msg;
                             }
-                            setupBracket(taxInfo);
-                            out.println("STORE: OK");
+                            boolean check = setupBracket(taxInfo);
+                            if(check) {
+                                out.println("STORE: OK");
+                            }else
+                                out.println("STORE: FULL");
                             msg = "";
                             break;
                         case "QUERY":
                             outputMessage((msg));
                             System.out.println(brackets.size());
-                            for(int i = 0; i < brackets.size(); i++){
+                            for (int i = 0; i < brackets.size(); i++) {
                                 out.println(brackets.get(i).outputFormat());
                             }
                             out.println("QUERY: OK");
@@ -71,63 +71,49 @@ public class ServerSample {
 
 
     public boolean setupBracket(String[] taxInfo) {
-        System.out.println("start checking");
-        TaxBracket temporary = new TaxBracket(taxInfo[0], taxInfo[1],taxInfo[2], taxInfo[3]);
-        if(brackets.size() == 0) {
+        TaxBracket temporary = new TaxBracket(taxInfo[0], taxInfo[1], taxInfo[2], taxInfo[3]);
+        if (brackets.size() == 0) {
             brackets.add(temporary);
             return true;
         }
-        for(int i = 0; i < brackets.size(); i++) {
+        for (int i = 0; i < brackets.size(); i++) {
             TaxBracket check = brackets.get(i);
             int checkNumber = brackets.get(i).inRange(temporary.getBottom(), temporary.getTop());
-            System.out.println(checkNumber+"is number");
-
             if (brackets.size() < 10) {
+
                 if (checkNumber == 0) {
                     if (i == brackets.size() - 1) {
-                        if(brackets.get(brackets.size()-1).getTop() < temporary.getBottom()) {
-                            System.out.println("check");
+                        if (brackets.get(brackets.size() - 1).getTop() < temporary.getBottom()) {
                             brackets.add(temporary);
                         }
                     }
                 }
                 if (checkNumber == 1) {
                     if (brackets.size() == i + 1) {
+                        if (brackets.get(i).getBottom() == 0) {
+                            brackets.set(i, temporary);
+                        } else {
+                            brackets.get(i).setTop(temporary.getBottom() - 1);
+                            brackets.add(temporary);
+                        }
+                    } else {
                         brackets.get(i).setTop(temporary.getBottom() - 1);
-                        brackets.add(temporary);
+                        brackets.add(i + 1, temporary);
                     }
-                    else
-                    {
+
+                } else if (checkNumber == 2) {
+                    brackets.get(i).setBottom(temporary.getTop() + 1);
+                    break;
+                } else if (checkNumber == 3) {
+                    if (brackets.get(i).getBottom() == temporary.getBottom()) {
+                        brackets.add(i, temporary);
+                        brackets.get(i + 1).setBottom(temporary.getTop() + 1);
+                        break;
+                    } else if (brackets.get(i).getTop() == temporary.getTop()) {
                         brackets.get(i).setTop(temporary.getBottom() - 1);
-                        brackets.add(i+1,temporary);
-                    }
-
-                }
-                else if (checkNumber == 2) {
-     /*               if (brackets.get(i).getTop() <= temporary.getTop()) {
-                        brackets.remove(i);
-                        brackets.add(i,temporary);
-                    } else {*/
-
-                        brackets.get(i).setBottom(temporary.getTop() + 1);
+                        brackets.add(i + 1, temporary);
                         break;
-//                        brackets.add(i, temporary);
-                    }
-
-                else if (checkNumber == 3) {
-                    System.out.println("3 in");
-                    if(brackets.get(i).getBottom() == temporary.getBottom()){
-                        brackets.add(i,temporary);
-                        brackets.get(i+1).setBottom(temporary.getTop()+1);
-                        break;
-                    }
-                    else if(brackets.get(i).getTop() == temporary.getTop())
-                    {
-                        brackets.get(i).setTop(temporary.getBottom()-1);
-                        brackets.add(i+1,temporary);
-                        break;
-                    }
-                   else if(brackets.size() < 9) {
+                    } else if (brackets.size() < 9) {
                         TaxBracket extra1 = new TaxBracket(
                                 String.valueOf(brackets.get(i).getBottom()),
                                 String.valueOf(brackets.get(i).getTop()),
@@ -140,28 +126,24 @@ public class ServerSample {
                         brackets.add(i + 1, temporary);
                         break;
                     }
-                }
-
-                else if (checkNumber == 4) {
+                } else if (checkNumber == 4) {
                     brackets.set(i, temporary);
-                }
-
-                else if(checkNumber == 5)
-                {
-                    System.out.println("in 5");
+                } else if (checkNumber == 5) {
                     brackets.remove(i);
                     i--;
                 }
 
-            }else {
+            } else {
                 if (brackets.get(i).inRange(temporary.getBottom(), temporary.getTop()) == 4) {
                     brackets.set(i, temporary);
                     break;
                 }
+                return false;
             }
         }
         return true;
     }
+
     public static void outputMessage(String msg) {
         System.out.format("CLIENT: %s\n", msg);
     }
